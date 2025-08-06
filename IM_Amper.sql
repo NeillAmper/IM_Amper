982202cc59e7c49e5c9a0fbfe7e239726dea3092
NeillAmper <namper_240000001770@uic.edu.ph>
2025-08-06 11:48:42 +0800

    Add files via upload

---
+++ IM_Amper.sql
@@ -0,0 +1,13 @@
+-- create database bank;
+
+
+-- create user 'neill'@'localhost' identified by 'xyz';
+
+
+-- grant all privileges on bank.* to 'neill'@'localhost';
+
+-- use bank;
+
+-- select current_user();
+
+select * from department
